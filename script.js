22c8faa08f92cf1ed2cf0121ae03b1f23179ca72
ProfesorRafaelBiano87 <biano.rafael@escola.pr.gov.br>
2025-07-03 14:42:45 +0000

    aula 2

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click',mostrapalavraschave);
+
+function mostrapalavraschave(){
+    alert('Deu boa Piazada');
+}
